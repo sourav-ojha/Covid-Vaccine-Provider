780e5cfbced925eed4c039c29942ba8e2e8ab3dc
sourav-ojha <subhamojha9@gmail.com>
2021-07-04 17:45:37 +0530

    home page maintained

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,19 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    var navBar = document.getElementById("navbar_top");
+    if (window.scrollY > 170) {
+      navBar.classList.add("fixed-top");
+      // add padding top to show content behind navbar
+      navbar_height = document.querySelector(".navbar").offsetHeight;
+      document.body.style.paddingTop = 110 + "px";
+      navBar.classList.remove("nav_norm");
+      navBar.classList.add("bg-danger");
+    } else {
+      navBar.classList.remove("fixed-top");
+      // remove padding top from body
+      document.body.style.paddingTop = "0";
+      navBar.classList.remove("bg-danger");
+      navBar.classList.add("nav_norm");
+    }
+  });
+});
